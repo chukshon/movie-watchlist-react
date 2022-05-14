1b1c76b990bda6b827c903f12ab3d3c146eee998
ChukwukaHonour <ChukwukaHonour@gmail.com>
2022-05-14 15:08:06 +0100

    add page started

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,7 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Add = () => {
-  return <div>Add</div>
+  const [value, setValue] = useState('')
+  const [results, setResults] = useState([])
+
+  const handleChange = async (e) => {
+    setValue(e.target.value)
+
+    try {
+      const api = await fetch(
+        `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=1&include_adult=false&query=${e.target.value}`
+      )
+      const data = await api.json()
+      setResults(data.results)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+  return (
+    <div className='add-page'>
+      <div className='container'>
+        <div className='add-content'>
+          <div className='input-wrapper'>
+            <input
+              type='text'
+              placeholder='Search for a movie'
+              value={value}
+              onChange={handleChange}
+            />
+          </div>
+          {results.map((movie) => {
+            return <h1 key={movie.id}>{movie.title}</h1>
+          })}
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Add
