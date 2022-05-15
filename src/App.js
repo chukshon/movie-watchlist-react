d7fff2b258d60e07339a4995474e30123c06a2d2
ChukwukaHonour <ChukwukaHonour@gmail.com>
2022-05-15 14:15:06 +0100

    route ish

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <Router>
       <Header />
       <Routes>
-        <Route path='/' element={<WatchList />} />
+        <Route index element={<WatchList />} />
         <Route path='/add' element={<Add />} />
         <Route path='/watched' element={<Watched />} />
       </Routes>
