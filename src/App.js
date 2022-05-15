96a7a5310f2f93641518fd11811385e1eef9ce3e
ChukwukaHonour <ChukwukaHonour@gmail.com>
2022-05-15 14:21:31 +0100

    fixes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Router, Route, Routes } from 'react-router-dom'
 import Header from './components/Header'
 import './App.css'
 import './lib/font-awesome/css/all.min.css'
@@ -8,15 +8,17 @@ import Watched from './pages/Watched'
 
 function App() {
   return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route index element={<WatchList />} />
-        <Route path='/add' element={<Add />} />
-        <Route path='/watched' element={<Watched />} />
-        <Route path='*' element={<h1>Error</h1>} />
-      </Routes>
-    </Router>
+    <BrowserRouter>
+      <Router>
+        <Header />
+        <Routes>
+          <Route index element={<WatchList />} />
+          <Route path='/add' element={<Add />} />
+          <Route path='/watched' element={<Watched />} />
+          <Route path='*' element={<h1>Error</h1>} />
+        </Routes>
+      </Router>
+    </BrowserRouter>
   )
 }
 
